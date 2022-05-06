f20745a9afebe7fc9a94ae592e8d9d462b9dac76
bsungwoo <61150422+bsungwoo@users.noreply.github.com>
2022-05-07 00:41:37 +0900

    Add find_packages

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name = "spSeudoMap",
-    version = "0.0.1",
+    version = "1.0.0",
     description = "Cell type mapping of spatial transcriptomics using unmatched single-cell RNA-seq data",
     url = "https://github.com/bsungwoo/spSeudoMap.git",
+    packages=find_packages(include=['spSeudoMap', 'spSeudoMap.*']),
     author = "Sungwoo Bae, Hongyoon Choi",
     install_requires = ["scanpy==1.5.1","pandas==1.3.5","numpy==1.21.6",
                         "h5py==2.10.0", "jupyter",
